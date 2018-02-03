d60f11cc3b7a3b7f4a2b09ddb3ec85e784edf2e5
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-04 11:12:34 +1300

    expect output to be pairs

--- example.js
+++ example.js
@@ -9,7 +9,7 @@ var ctx = canvas.getContext('2d')
 var spline = require('./')
 
 var input = []
-for(var i = 0; i < 10; i++) {
+for(var i = 0; i < 20; i++) {
   input.push([Math.random()*canvas.width, Math.random()*canvas.height])
 }
 
@@ -18,19 +18,19 @@ input.forEach(function (p) {
   _input.push(p[0]); _input.push(p[1])
 })
 
-var a = spline(_input, 0.5, 10)
+var a = spline(_input, 0.5, 100)
 
 ctx.beginPath()
 for(var i = 0; i < a.length; i+=2) {
-  if(i) ctx.lineTo(a[i], a[i+1])
-  else ctx.moveTo(a[i], a[i+1])
+  if(i) ctx.lineTo(a[i][0], a[i][1])
+  else ctx.moveTo(a[i][0], a[i][1])
 }
 ctx.stroke()
 
 ctx.fillStyle = 'red'
 for(var i = 0; i < input.length; i++)
-  ctx.fillRect(input[i][0],input[i][1], 10, 10)
-ctx.stroke()
+  ctx.fillRect(~~input[i][0]-3,~~input[i][1]-3, 6, 6)
+//ctx.stroke()
 
 console.log(input)
 
